8df09fc5e6f1f800a37d7d0f9d7e824ac2e9edad
Loganator4625 <Loganator4625@users.noreply.github.com>
2016-05-11 17:44:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function startGame() {
   document.getElementById("startButton").className = "hidden";
   document.getElementById("background").className = "blackBackground"
+  document.getElementById("welcomingHeading").className = "hidden";
+  
 }
